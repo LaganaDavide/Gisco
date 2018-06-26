0cfd552b367c3f5109a54871a15cab1ce9b5598c
LaganaDavide <lagana.davide89@gmail.com>
2018-06-26 11:50:15 +0200

    cambio nome cartella

--- AllInApp_App/src/app/models/user/user.namespace.ts
+++
@@ -1,7 +0,0 @@
-export namespace User {
-    export class UserData {
-        public token: string;        
-        public username: string;        
-        public password: string;      
-    }
-}
